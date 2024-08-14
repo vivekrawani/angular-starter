04cc7132e52770fa2fb108f462b3919fe590ea61
Vivek <kumarrawanivivek@gmail.com>
2024-08-14 16:23:43 +0530

    dock

---
+++ src/app/components/desktop-icon/desktop-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './desktop-icon.component.html',
+  styleUrl: './desktop-icon.component.css'
+})
+export class DesktopIconComponent {
+
+}
