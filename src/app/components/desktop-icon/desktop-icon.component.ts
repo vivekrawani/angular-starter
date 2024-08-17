415bb43dc83ab17feb22d5d7f46744030195dca3
vivek <kumarrawanivivek@gmail.com>
2024-08-17 15:48:12 +0530

    modal added

--- src/app/components/desktop-icon/desktop-icon.component.ts
+++ src/app/components/desktop-icon/desktop-icon.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input } from "@angular/core";
 import { DesktopIcon } from "../../interfaces/desktop-icon";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { matClose } from "@ng-icons/material-icons/baseline";
 @Component({
   selector: "app-desktop-icon",
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: "./desktop-icon.component.html",
   styleUrl: "./desktop-icon.component.css",
+  viewProviders: [provideIcons({ matClose })],
 })
 export class DesktopIconComponent {
   @Input() iconDetails!: DesktopIcon;
-  @Input() showFileName !: Boolean;
+  @Input() showFileName!: Boolean;
+  isModalOpen = false;
   onDoubleClick() {
-    alert("window");
+    this.isModalOpen = true;
+  }
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
