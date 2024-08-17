2b1529b5eb4c50d9957ed9ebe5fe7299be75d74b
vivek <kumarrawanivivek@gmail.com>
2024-08-17 20:19:54 +0530

    dynamic modal load

--- src/app/components/desktop-icon/desktop-icon.component.ts
+++ src/app/components/desktop-icon/desktop-icon.component.ts
@@ -2,10 +2,25 @@ import { Component, Input } from "@angular/core";
 import { DesktopIcon } from "../../interfaces/desktop-icon";
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
 import { matClose } from "@ng-icons/material-icons/baseline";
+import { EducationComponent } from "../education/education.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
+import { TechComponent } from "../tech/tech.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { NotFoundComponent } from "../not-found/not-found.component";
+import { NgComponentOutlet } from "@angular/common";
+
 @Component({
   selector: "app-desktop-icon",
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [
+    NgIconComponent,
+    AboutMeComponent,
+    TechComponent,
+    ProjectsComponent,
+    NotFoundComponent,
+    EducationComponent,
+    NgComponentOutlet,
+  ],
   templateUrl: "./desktop-icon.component.html",
   styleUrl: "./desktop-icon.component.css",
   viewProviders: [provideIcons({ matClose })],
@@ -19,5 +34,29 @@ export class DesktopIconComponent {
   }
   closeModal() {
     this.isModalOpen = false;
+  }
+  getComponent() {
+    switch (this.iconDetails.fileName) {
+      case "About me": {
+        return AboutMeComponent;
+        break;
+      }
+      case "Education": {
+        return EducationComponent;
+        break;
+      }
+      case "Tech": {
+        return TechComponent;
+        break;
+      }
+      case "Projects": {
+        return ProjectsComponent;
+        break;
+      }
+      default: {
+        return NotFoundComponent;
+        break;
+      }
+    }
   }
 }
---
+++ src/app/components/tech/tech.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tech',
+  standalone: true,
+  imports: [],
+  templateUrl: './tech.component.html',
+  styleUrl: './tech.component.css'
+})
+export class TechComponent {
+
+}
