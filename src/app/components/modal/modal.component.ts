aa981286ff9697e68e517b8c67db806a47e68311
vivek <kumarrawanivivek@gmail.com>
2024-08-19 23:32:54 +0530

    make modal moveable

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,12 @@
-import { Component, Output, EventEmitter, Input } from "@angular/core";
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  HostListener,
+  ElementRef,
+  Renderer2,
+} from "@angular/core";
 import { DesktopIcon } from "../../interfaces/desktop-icon";
 import { EducationComponent } from "../education/education.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
@@ -52,5 +60,57 @@ export class ModalComponent {
         break;
       }
     }
+  }
+
+  private isDragging = false;
+  private startX = 0;
+  private startY = 0;
+  private initialX = 0;
+  private initialY = 0;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+  ) {}
+
+  onMouseDown(event: MouseEvent): void {
+    // Make sure the click is on the border for resizing, otherwise enable dragging
+    const element = this.el.nativeElement.querySelector(
+      "#movable-resizable-div",
+    );
+    const rect = element.getBoundingClientRect();
+    if (
+      event.target === element &&
+      event.offsetX > rect.width - 10 &&
+      event.offsetY > rect.height - 10
+    ) {
+      return;
+    }
+
+    this.isDragging = true;
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.initialX = element.offsetLeft;
+    this.initialY = element.offsetTop;
+
+    this.renderer.listen("document", "mousemove", this.onMouseMove.bind(this));
+    this.renderer.listen("document", "mouseup", this.onMouseUp.bind(this));
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    if (!this.isDragging) return;
+
+    const deltaX = event.clientX - this.startX;
+    const deltaY = event.clientY - this.startY;
+
+    const element = this.el.nativeElement.querySelector(
+      "#movable-resizable-div",
+    );
+    element.style.left = `${this.initialX + deltaX}px`;
+    element.style.top = `${this.initialY + deltaY}px`;
+  }
+
+  onMouseUp(): void {
+    this.isDragging = false;
   }
 }
